fe824364196d44be7aea9ab4d166bc77459546e2
ANURAG24TIWARI <24tiwarianu@gmail.com>
2023-09-08 14:47:31 +0530

    adding all files in github

---
+++ arithemetic.py
@@ -0,0 +1,13 @@
+# arithemetic operation
+a = 58
+b = 100
+add = a + b
+sub = a - b
+div = a / b
+mul = a * b
+
+print('Arthematic operation of number 58 and 100')
+print('addition ={}' .format(add))
+print('subtraction ={}' .format(sub))
+print('division ={}' .format(div))
+print('multiplication ={}' .format(mul))
---
+++ list.py
@@ -0,0 +1,11 @@
+# number greater than 50
+num_list = [24,100,51,32,12]
+
+print(" Number in list :")
+for i in num_list:
+    print(i)
+
+print ("Number greater than 50 are :")
+for n in num_list:
+    if n>50:
+        print(n)
